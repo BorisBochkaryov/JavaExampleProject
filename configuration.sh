3e83a50fd52e30162db7905d3e91f3c628a7ec80
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2019-01-25 02:01:42 +0700

    Скрипт для работы с БД MySQL и дамп

---
+++ configuration.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+case $1 in
+    load )
+        dialog --title "Setting for project" --clear --inputbox "MySQL username" -1 -1 2>/tmp/java_project_example
+        response=$?
+        case $response in
+            0)
+                answer=$(<"/tmp/java_project_example")
+                mysql -u "$answer" -p -e "CREATE DATABASE phonebook;"
+                mysql -u "$answer" -p phonebook < phonebook.sql
+            ;;
+        esac
+        rm /tmp/java_project_example
+    ;;
+    save )
+        dialog --title "Setting for project" --clear --inputbox "MySQL username" -1 -1 2>/tmp/java_project_example
+        response=$?
+        case $response in
+            0)
+                username=$(<"/tmp/java_project_example")
+                mysqldump --opt phonebook -u"$username" -p > phonebook.sql
+            ;;
+        esac
+        rm /tmp/java_project_example
+    ;;
+    * )
+
+    ;;
+esac
